f62081988ac428d206c6f7915065642a33998291
Javier Bórquez <hi@javier.xyz>
2016-06-21 00:21:11 -0500

    Add baby animatino

--- views/Home.js
+++ views/Home.js
@@ -4,12 +4,30 @@ const ReactNative = require('react-native')
 const {View, Text, Image} = ReactNative
 
 const Home = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Image source={require('../assets/bebe1.png')} />
-      </View>
-    )
+  getInitialState() {
+    return {
+      currentSprite: 1
+    }
+  },
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        currentSprite: this.state.currentSprite === 1 ? 2 : 1
+      })
+    }, 500)
+  },
+
+  render() {
+    const {currentSprite} = this.state
+
+    return <View style={styles.container}>
+      {currentSprite === 1 ? (
+        <Image style={{width: 300, height: 300}} resizeMode='contain' source={require('../assets/bebe1.png')} />
+      ) : (
+        <Image style={{width: 300, height: 300}} resizeMode='contain' source={require('../assets/bebe2.png')} />
+      )}
+    </View>
   }
 })
 
